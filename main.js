dffbbb3e7d610c5982fdf880238cbececd9dc8c4
Oxana Ciobanu <oxanochka23@yahoo.com>
2020-04-21 17:50:22 +0300

    javascript commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let userName ='Oxana';
+
+
+alert(userName); 
+console.log( typeof userName);
+
+console.log( typeof 100);
+
+
+let x =30;
+let y =10;
+let z;
+console.log( z );
+
+let age =null;
+console.log( age);
+
+
+let film =false;
+film = true;
+console.log( typeof film);
+
+
